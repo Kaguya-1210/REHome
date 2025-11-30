29d52294cd049d7d86f05ecd5d82f6ee84623a45
REHomeUser <rider0@163.com>
2025-11-30 16:11:20 +0800

    feat(Admin): 添加管理员登录界面和样式
    添加管理员登录卡片组件并调整页面布局样式

--- REHomeWeb/src/views/Admin/REHomeAdmin.vue
+++ REHomeWeb/src/views/Admin/REHomeAdmin.vue
@@ -3,15 +3,44 @@
 <template>
   <div class="common-layout"> 
  
-    <el-container style="">
+    <el-container >
       <el-header style="background-color: white;  height: 10vh;">
-        Admin
+        <div class="logo" >
+        <el-text type="Default" style="font-size: 200%;" >REHomeAdmin</el-text>
+        </div>
       </el-header>
-      <el-main style="background-color: rgb(224, 226, 228); height: 90vh;">Main</el-main>
+      <el-main style="background-color: rgb(224, 226, 228); height: 90vh;position: relative;">
+        <div class="loginCard" >
+        <el-card style="width: 80%; height: 90%;border-radius: 10px;text-align: center;">
+            <template #header style="display: flex;">
+              <el-text type="Default" style="font-size: 150%;">管理员登录</el-text>
+            </template>
+            
+        </el-card>
+        </div>
+      </el-main>
     </el-container>
      </div>
 </template>
 
 <style scoped>
-
+.logo {
+  color: white;
+  /* background-color: black; */
+  height: 100%;
+  width: 15%;
+  display: flex;
+  justify-content: center;
+  padding-left: 2%;
+}
+.loginCard {
+   position: absolute;
+   display: flex;
+   justify-content: center; 
+   /* background-color: black; */
+   width: 30%;
+   height: 60%;
+   right: 5%;
+   bottom: 20%;
+}
 </style>
