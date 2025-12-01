63a5068ef1530ecf1242b317b6dab1ab8ee4d941
REHomeUser <rider0@163.com>
2025-12-01 15:20:04 +0800

    feat: 添加MybatisPlus配置及依赖
    添加MybatisPlus配置类并引入相关依赖，用于支持MybatisPlus的功能集成

--- REhome/src/main/java/w7insvnter/com/rehome/REhomeApplication.java
+++ REhome/src/main/java/w7insvnter/com/rehome/REhomeApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class REhomeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(REhomeApplication.class, args);
     }
