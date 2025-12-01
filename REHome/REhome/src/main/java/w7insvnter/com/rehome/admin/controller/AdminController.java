e438a7873c2795170d73c3f3d50c5eb5a188f366
REHomeUser <rider0@163.com>
2025-12-01 19:07:14 +0800

    refactor: 移除未使用的导入和@Data注解
    清理AdminInfo和AdminController类中未使用的导入语句
    将AdminInfo类中的@Data注解替换为@Getter

--- REHome/REhome/src/main/java/w7insvnter/com/rehome/admin/controller/AdminController.java
+++ REHome/REhome/src/main/java/w7insvnter/com/rehome/admin/controller/AdminController.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import w7insvnter.com.rehome.admin.service.AdminService;
-import org.springframework.web.bind.annotation.GetMapping;
 import w7insvnter.com.rehome.admin.pojo.entity.AdminInfo;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
