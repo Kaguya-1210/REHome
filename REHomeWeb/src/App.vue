3fe5aa6e81e21c94f16825382cf61486e266509f
REHomeUser <rider0@163.com>
2025-12-13 15:30:42 +0800

    docs: 更新页脚中的作者名称

--- REHomeWeb/src/App.vue
+++ REHomeWeb/src/App.vue
@@ -7,7 +7,7 @@
     <!-- 全局页脚 - 脱离当前布局，固定在屏幕正下方 -->
     <el-footer style="position: fixed; left: 50%; bottom: 10vh; transform: translateX(-50%);">
       <div style="text-align: center;">
-        <el-text type="danger">Test<br>version:0.0.1<br>2025-11-30<br>by W7insvnter(冬七柒) & TraeCN</el-text>
+        <el-text type="danger">Test<br>version:0.0.1<br>2025-11-30<br>by W7insvnter(冬七柒) & Trae</el-text>
       </div>
     </el-footer>
   </div>
